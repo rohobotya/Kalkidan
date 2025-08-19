e40fdbf7abc6463b699c79f88d595ea1ee653327
rohobotya <yaikobrohobot@gmail.com>
2025-08-19 21:53:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace with your Firebase Config
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBb8tKHIA67DM9t552t8q-plK4ketrA3lg",
+  authDomain: "zowibranding.firebaseapp.com",
+  databaseURL: "https://zowibranding-default-rtdb.firebaseio.com",
+  projectId: "zowibranding",
+  storageBucket: "zowibranding.firebasestorage.app",
+  messagingSenderId: "43545840966",
+  appId: "1:43545840966:web:35f96642aff40288a1a947",
+  measurementId: "G-PYM4P96BXB"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
 // Handle form submission
